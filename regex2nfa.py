ede68b94b36b434abd801f6995779f48af94595e
Shubahm shukla <shubhamshuklaerssss@gmail.com>
2015-01-18 09:34:13 +0530

    Added ? functionality and tested

--- regex2nfa.py
+++ regex2nfa.py
@@ -9,7 +9,7 @@ class regex2nfa(QtCore.QObject):
     
     def __init__(self,regex):
         super(QtCore.QObject,self).__init__()
-        self.operators=["+","(",")","*","&"]
+        self.operators=["+","(",")","*","&","?"]
         self.alphabets=['x','y']
         self.regex=regex
         self.nfa=automata("nfa")
@@ -61,6 +61,16 @@ class regex2nfa(QtCore.QObject):
                     logging.warning(final_state)
 
                     stack.append((initial_state,final_state))
+                elif (char == "?"):
+                    #popping the operand
+                    temp_tuple=stack.pop()
+                    initial_state=temp_tuple[0]
+                    final_state=temp_tuple[1]
+                    
+                    self.nfa.add_transition(initial_state,epsilon,final_state)
+
+                    stack.append((initial_state,final_state))
+
                 elif (char == "+"):
                     self.nfa.add_state()
                     new_state+=1
@@ -125,7 +135,7 @@ class regex2nfa(QtCore.QObject):
 
 
     def heigher_precidence(self,a,b):
-        if( a == "*" ):
+        if( a == "*" or a == "?"):
             return True
         elif( a == "&" and b == "+" ):
             return True
@@ -138,7 +148,7 @@ class regex2nfa(QtCore.QObject):
 
         for char in self.regex:
             if temp_regex != "":
-                if temp_regex[-1] in self.alphabets or temp_regex[-1] == ")" or temp_regex[-1] == "*":
+                if temp_regex[-1] in self.alphabets or temp_regex[-1] == ")" or temp_regex[-1] == "*" or temp_regex[-1] == "?":
                     if char in self.alphabets or char == "(":
                         temp_regex+="&"+char
                     else:
